c274ef30e25aad0d830dfa7f12ec8f2b343eab9b
iamnars <narene.nagares@gmail.com>
2025-02-13 13:34:13 +0800

    Refactor layout: Moved sidebar and top navigation to reusable Blade partials for consistency

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav metismenu" id="side-menu">
+                    <li class="nav-header">
+                        <div class="dropdown profile-element">
+                            <img alt="image" class="rounded-circle" src="img/profile_small.jpg"/>
+                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                                <span class="block m-t-xs font-bold">David Williams</span>
+                                <span class="text-muted text-xs block">BSIT Professor <b class="caret"></b></span>
+                            </a>
+                            <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                                <li><a class="dropdown-item" href="profile.html">Profile</a></li>
+                                <li><a class="dropdown-item" href="contacts.html">Contacts</a></li>
+                                <li><a class="dropdown-item" href="mailbox.html">Mailbox</a></li>
+                                <li class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="login.html">Logout</a></li>
+                            </ul>
+                        </div>
+                        <div class="logo-element">
+                            <img src="img/mpc_logo.png" style="width: 100%; max-width: 50px; height: auto;">
+                        </div>
+                    </li>
+                    <li class="active">
+                        <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Classes Handled</span> <span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li class="active"><a href="{{ url('/grading') }}">CC100IT1A124</a></li>
+                            <li><a href="dashboard_2.html">CC101IT1A124</a></li>
+                            <li><a href="dashboard_3.html">CC103IT2A124</a></li>
+                            <li><a href="dashboard_4_1.html">ELEC2IT2A124</a></li>
+                            <li><a href="dashboard_5.html">IM102IT3A124</a></li>
+                        </ul>
+                    </li>
+                    
+
+            </div>
+        </nav>
