f33879aad7c89c7cfcfcbee1a3397cc3e5a98be2
iamnars <narene.nagares@gmail.com>
2025-02-13 15:31:52 +0800

    Draft of migration for enrollments, schedules and subjects tables.

--- database/migrations/2025_02_13_055646_create_enrollments_table.php
+++ database/migrations/2025_02_13_055646_create_enrollments_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id(); // Auto-increment primary key
-            $table->string('sched_id');
-            $table->string('student_id');
+            $table->foreign('student_number')->references('student_number')->on('students')->onDelete('set null');
+            $table->foreignId('schedule_code')->references('schedule_code')->on('schedules')->onDelete('set null');
             $table->integer('semester');
-            $table->string('sy', 9);
+            $table->string('school_year', 9);
             $table->enum('status', ['enrolled', 'dropped', 'pending', 'completed'])->default('enrolled');
             $table->timestamp('enrolled_at')->nullable();
             $table->text('remarks')->nullable();
